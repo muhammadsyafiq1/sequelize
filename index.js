c1b320ced842d38304e60f9354bd99d3e2420373
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2022-12-22 13:19:32 +0700

    models and migrate

--- index.js
+++ index.js
@@ -4,10 +4,53 @@ const sequelize = new Sequelize('sequelize','root', '', {
     dialect: 'mysql'
 });
 
-sequelize.authenticate().then(() => {
-    console.log('Connection successfully');
-}).catch((err) => {
-    console.log('Error ya gaess');
+// #1 connection
+// sequelize.authenticate().then(() => {
+//     console.log('Connection successfully');
+// }).catch((err) => {
+//     console.log('Error ya gaess');
+// });
+
+// console.log("Another task"); 
+
+// #2 Models
+const User = sequelize.define('users', {
+    username: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING
+    },
+    age: {
+        type: Sequelize.DataTypes.INTEGER,
+        defaultValue: 21
+    },
+    WittCodeRocks :{
+        type: Sequelize.DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+}, {
+    freezeTableName: true
 });
 
-console.log("Another task");
+// migrate table
+// User.sync().then((data) => {
+//     console.log('Success');
+// }).catch((err) => {
+//     console.log('error');
+// });
+
+// drop tabel jika ada dan create table
+// User.sync({force: true}).then((data) => {
+//     console.log('Success');
+// }).catch((err) => {
+//     console.log('error');
+// });
+
+// tambah atau hapus field table
+User.sync({alter: true}).then((data) => {
+    console.log('Success');
+}).catch((err) => {
+    console.log('error');
+});
