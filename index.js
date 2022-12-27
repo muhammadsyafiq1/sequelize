fb9fa73395b7d8bcfa37ae6bf4a6388319abb086
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2022-12-27 08:33:08 +0700

    sequelize

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize")
+const { Sequelize, Op } = require("sequelize")
 const {DataTypes} = Sequelize
 
 const sequelize = new Sequelize('sequelize','root', '', {
@@ -15,28 +15,30 @@ const sequelize = new Sequelize('sequelize','root', '', {
 // console.log("Another task"); 
 
 // #2 Models
-const User = sequelize.define('users', {
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate:{
-            len: [3, 16]
-        }
-    },
-    password: {
-        type: DataTypes.STRING
-    },
-    age: {
-        type: DataTypes.INTEGER,
-        defaultValue: 21
-    },
-    WittCodeRocks :{
-        type: DataTypes.BOOLEAN,
-        defaultValue: true
-    }
-}, {
-    freezeTableName: true
-});
+// const User = sequelize.define('users', {
+//     username: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate:{
+//             len: [3, 16]
+//         }
+//     },
+//     password: {
+//         type: DataTypes.STRING
+//     },
+//     age: {
+//         type: DataTypes.INTEGER,
+//         defaultValue: 21
+//     },
+//     WittCodeRocks :{
+//         type: DataTypes.BOOLEAN,
+//         defaultValue: true
+//     }
+// }, {
+//     freezeTableName: true
+// });
+
+// -------------------------------------------------models migrate
 
 // migrate table
 // User.sync().then((data) => {
@@ -59,6 +61,8 @@ const User = sequelize.define('users', {
 //     console.log('error');
 // });
 
+// ----------------------------------------------------model querying
+
 // update table and create data
 // User.sync({alter: true}).then(() => {
 //     const user = User.build({username: "syafiq", password: "123", age: 25, WittCodeRocks: true});
@@ -69,7 +73,7 @@ const User = sequelize.define('users', {
 //     console.log(err);
 // })
 
-//bulk create
+//bulk create & test validation bulk create
 // User.sync({alter: true}).then(()=>{
 //     return User.bulkCreate([
 //         {
@@ -108,13 +112,120 @@ const User = sequelize.define('users', {
 //     console.log(err);
 // })
 
-//all data
-User.sync({alter: true}).then(() => {
-    return User.findAll();
-}).then((data) => {
-    data.forEach((element) => {
-        console.log(element.toJSON());
-    })
+//all data with attribute
+// User.sync({alter: true}).then(() => {
+//     return User.findAll({
+        // SUM, AVG
+        // EXCLUDE UNTUK HIDE COLUMN
+//         attributes: [[sequelize.fn('AVG', sequelize.col('age')), 'Jumlah umur']]
+//     });
+// }).then((data) => {
+//     data.forEach((element) => {
+//         console.log(element.toJSON());
+//     })
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+//where filter
+// User.sync({alter: true}).then(() => {
+    // return User.findAll({ attributes: ['username'], where: {age: 18}});
+//     return User.findAll({where: {age: 23, username: 'jeruk'}});
+// }).then((data) => {
+//     data.forEach((element) => {
+//         console.log(element.toJSON());
+//     })
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+
+//limit
+// User.sync({alter: true}).then(() => {
+//     return User.findAll({limit: 1});
+// }).then((data) => {
+//     data.forEach((element) => {
+//         console.log(element.toJSON());
+//     })
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+//group by
+// User.sync({alter: true}).then(() => {
+//     return User.findAll({
+//         attributes: ['username',
+//                     [sequelize.fn('SUM', sequelize.col('age')), 'sum_age']],
+//     group: 'username'});
+// }).then((data) => {
+//     data.forEach((element) => {
+//         console.log(element.toJSON());
+//     })
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+//group by with Op sequelize
+// User.sync({alter: true}).then(() => {
+//     return User.findAll({
+//         where :{
+            // gt = greater cari nilai lebih besar
+            // age: {
+            //     [Op.gt]: 18
+            // }
+
+            //or sambil semua data yg ada kesesuaian dengan filter
+            // and ambil data lebih spesifik
+            // [Op.and]: {username: 'b', age: 18}
+//         }
+//     });
+// }).then((data) => {
+//     data.forEach((element) => {
+//         console.log(element.toJSON());
+//     })
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+
+//--------------------------------------------------------relationships
+
+const Country = sequelize.define('country', {
+    countryName: {
+        type: DataTypes.STRING,
+        unique: true
+    }
+}, {
+    timestamps: false
+});
+
+const Capital = sequelize.define('capital', {
+    capitalName :{
+        type: DataTypes.STRING,
+        unique: true
+    }
+}, {
+    timestamps: false
+});
+
+//nantinya ada field countryId di tabel capital - otomatis
+Country.hasOne(Capital);
+// bisa didefinisikan
+// Country.hasOne(Capital, {foreignKey: 'soccer'});
+
+sequelize.sync({alter: true}).then(() => {
+    // return Country.bulkCreate([
+    //     {
+    //         capitalName : 'spain'
+    //     },
+    //     {
+    //         capitalName: 'germany'
+    //     },
+    //     {
+    //         capitalName : 'indonesia'
+    //     }
+    // ]);
 }).catch((err) => {
     console.log(err);
-});
+})
+
